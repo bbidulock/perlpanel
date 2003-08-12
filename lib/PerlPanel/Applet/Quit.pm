6065a8682216cd9b8975a8d16a80469f88a7bc3b
jodrell <jodrell>
2003-08-12 16:03:14 +0000

    added full copyright notices to all files, for Savannah application.

--- lib/PerlPanel/Applet/Quit.pm
+++ lib/PerlPanel/Applet/Quit.pm
@@ -1,4 +1,20 @@
-# $Id: Quit.pm,v 1.6 2003/06/10 13:30:05 jodrell Exp $
+# $Id: Quit.pm,v 1.7 2003/08/12 16:03:14 jodrell Exp $
+# This file is part of PerlPanel.
+# 
+# PerlPanel is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# PerlPanel is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with PerlPanel; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 package PerlPanel::Applet::Quit;
 use strict;
 
