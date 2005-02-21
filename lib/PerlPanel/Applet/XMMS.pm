73e10caf074e589f30ed44a44286006938258c88
jodrell <jodrell>
2005-02-21 15:56:37 +0000

    patch from Marc Brockschmidt to remove the volume button, since we have a separate volume applet now.

--- lib/PerlPanel/Applet/XMMS.pm
+++ lib/PerlPanel/Applet/XMMS.pm
@@ -1,4 +1,4 @@
-# $Id: XMMS.pm,v 1.17 2005/01/21 18:14:15 jodrell Exp $
+# $Id: XMMS.pm,v 1.18 2005/02/21 15:56:37 jodrell Exp $
 # This file is part of PerlPanel.
 # 
 # PerlPanel is free software; you can redistribute it and/or modify
@@ -27,7 +27,6 @@ our %TOOLTIPS = (
 	play	=> _('Play'),
 	pause	=> _('Pause'),
 	next	=> _('Play Next Track'),
-	volume	=> _('Adjust Volume'),
 );
 
 our %CALLBACKS = (
@@ -35,7 +34,6 @@ our %CALLBACKS = (
 	stop	=> sub { $_[0]->{controller}->stop },
 	play	=> sub { if ($_[0]->{controller}->is_playing) { $_[0]->{controller}->pause } else { $_[0]->{controller}->play }},
 	next	=> sub { $_[0]->{controller}->playlist_next },
-	volume	=> sub { $_[0]->toggle_volume_window },
 );
 
 our $ICON_DIR = sprintf('%s/share/%s/applets/xmms', $PerlPanel::PREFIX, lc($PerlPanel::NAME));
@@ -71,27 +69,13 @@ sub configure {
 	$self->{widget} = Gtk2::HBox->new;
 	$self->{controller} = Xmms::Remote->new;
 	$self->{pbfs}{pause} = Gtk2::Gdk::Pixbuf->new_from_file_at_size(sprintf('%s/%s.png', $ICON_DIR, 'pause'), PerlPanel::icon_size, PerlPanel::icon_size);
-	foreach my $name (qw(prev play stop next volume)) {
+	foreach my $name (qw(prev play stop next)) {
 		$self->{buttons}{$name} = $self->create_button($name);
 		my $func = $CALLBACKS{$name};
 		$self->{buttons}{$name}->signal_connect('clicked', sub { &$func($self) });
 		PerlPanel::tips->set_tip($self->{buttons}{$name}, $TOOLTIPS{$name});
 		$self->{widget}->pack_start($self->{buttons}{$name}, 0, 0, 0);
 	}
-	$self->{volume_slider} = Gtk2::VScale->new_with_range(0, 100, 1);
-	$self->{volume_slider}->set_draw_value(0);
-	$self->{volume_slider}->signal_connect('value_changed', sub {
-		$self->{volume_pending} = $self->{volume_slider}->get_value;
-		$self->{volume_slider_at_rest} = 0;
-	});
-	my $vbox = Gtk2::VBox->new;
-	$vbox->pack_start(Gtk2::Label->new('-'),  0, 0, 0);
-	$vbox->pack_start($self->{volume_slider}, 1, 1, 0);
-	$vbox->pack_start(Gtk2::Label->new('+'),  0, 0, 0);
-	$self->{volume_window} = Gtk2::Window->new('popup');
-	$self->{volume_window}->add(Gtk2::Viewport->new);
-	$self->{volume_window}->child->set_shadow_type('out');
-	$self->{volume_window}->child->add($vbox);
 	PerlPanel::add_timeout(50, sub {
 		my $running = 0;
 		eval('$running = ($self->{controller}->is_running ? 1 : 0)');
@@ -112,11 +96,6 @@ sub configure {
 				$self->{buttons}{stop}->set_sensitive(0);
 				$self->{buttons}{play}->child->set_from_pixbuf($self->{pbfs}{play});
 			}
-			if ($self->{volume_slider_at_rest} == 1 && defined($self->{volume_pending})) {
-				$self->{controller}->set_main_volume($self->{volume_pending});
-				undef($self->{volume_pending});
-			}
-			$self->{volume_slider_at_rest} = 1;
 			return 1;
 		}
 	});
@@ -127,42 +106,11 @@ sub configure {
 sub create_button {
 	my ($self, $id) = @_;
 	$self->{pbfs}{$id} = Gtk2::Gdk::Pixbuf->new_from_file_at_size(sprintf('%s/%s.png', $ICON_DIR, $id), PerlPanel::icon_size, PerlPanel::icon_size);
-	my $button;
-	if ($id eq 'volume') {
-		$button = Gtk2::ToggleButton->new;
-	} else {
-		$button = Gtk2::Button->new;
-	}
+	my $button = Gtk2::Button->new;
 	my $image = Gtk2::Image->new_from_pixbuf($self->{pbfs}{$id});
 	$button->set_relief('none');
 	$button->add($image);
 	return $button;
-}
-
-sub toggle_volume_window {
-	my $self = shift;
-	if ($self->{buttons}{volume}->get_active) {
-
-		$self->{volume_slider}->set_value($self->{controller}->get_main_volume);
-		$self->{volume_slider_at_rest} = 0;
-
-		$self->{volume_window}->show_all;
-		$self->{volume_window}->set_size_request($self->{buttons}{volume}->allocation->width, $SLIDER_SIZE);
-
-		my ($x, $y) = PerlPanel::get_widget_position($self->{buttons}{volume});
-
-		if (PerlPanel::position eq 'top') {
-			$y = PerlPanel::panel->allocation->height;
-		} else {
-			$y = PerlPanel::screen_height -
-					PerlPanel::panel->allocation->height -
-					$SLIDER_SIZE;
-		}
-		$self->{volume_window}->move($x, $y);
-	} else {
-		$self->{volume_window}->hide_all;
-	}
-	return 1;
 }
 
 sub widget {
